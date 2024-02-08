9e88e296b9dea1490c40065db1462bba40317727
Arpita karki <151529261+Arpitakarki@users.noreply.github.com>
2024-02-08 12:26:49 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function myFun() {
+    let java = document.getElementById("arpita")
+    if (java.style.display ==="none"){
+        java.style.display ="block";
+    }
+    else{
+        java.style.display ="none";
+    }
+}
